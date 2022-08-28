413e333ceb74a1c7bb4c69fe532f19de11e33439
Abhay Kant Tiwari <tabhaykant2@gmail.com>
2022-08-27 20:07:58 -0700

    footer and some data

--- enterprise.js
+++ enterprise.js
@@ -3,11 +3,9 @@ let playIcon=document.querySelectorAll(".playIcon")
 for(let i=0;i<viedo.length;i++){
     viedo[i].addEventListener("mouseover",function(){
         viedo[i].play();
-        playIcon[i].style.display="none"
     })
     viedo[i].addEventListener("mouseout",function(){
         viedo[i].pause();
-        playIcon[i].style.display="block"
     })
 }
 
